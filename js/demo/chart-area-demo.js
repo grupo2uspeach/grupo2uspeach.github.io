e70a0cab86e744bf706bf4f62af2a23263d56639
Hugo Bechtold Curi <hugo.curi@amparosaude.com>
2019-11-17 20:11:01 -0300

    initial commit

---
+++ js/demo/chart-area-demo.js
@@ -0,0 +1,54 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Nov/19", "Dez/19", "Jan/19", "Fev/19", "Mar/19", "Abr/19", "Mai/19", "Jun/19", "Jul/19", "Ago/19", "Set/19", "Out/19", "Nov/19"],
+    datasets: [{
+      label: "Média da performance",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [1.0000, 3.0162, 2.6263, 1.8394, 1.8287, 2.8682, 3.1274, 3.3259, 2.5849, 2.4159, 3.2651, 3.1984, 3.8451],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 13
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 5,
+          maxTicksLimit: 6
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
